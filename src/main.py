2d99650b97d174b8a24b52f3b9458fa11a3ef5d6
German Tarasov <g.tarasov@tinkoff.ru>
2025-10-26 19:32:49 +0300

    lint added

--- src/main.py
+++ src/main.py
@@ -8,14 +8,22 @@ logger = logging.getLogger(__name__)
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Обработка аргументов.")
-    parser.add_argument("task_type", type=str, help="Тип решаемой задачи", choices=['generate', 'solve'])
+    parser.add_argument(
+        "task_type", type=str, help="Тип решаемой задачи", choices=["generate", "solve"]
+    )
     parser.add_argument("--algorithm", type=str, help="Второе слово")
     parser.add_argument("--width", type=int, help="Ширина лабиринта", default=None)
     parser.add_argument("--height", type=int, help="Длина лабиринта", default=None)
-    parser.add_argument("--file", type=str, help="Файл с описанием лабиринта", default=None)
-    parser.add_argument("--start", type=str, help="Начальная точка маршрута", default=None)
+    parser.add_argument(
+        "--file", type=str, help="Файл с описанием лабиринта", default=None
+    )
+    parser.add_argument(
+        "--start", type=str, help="Начальная точка маршрута", default=None
+    )
     parser.add_argument("--end", type=str, help="Конечная точка маршрута", default=None)
-    parser.add_argument("--output", type=str, help="Путь для сохранения лабиринта", default=None)
+    parser.add_argument(
+        "--output", type=str, help="Путь для сохранения лабиринта", default=None
+    )
 
     args = parser.parse_args()
 
